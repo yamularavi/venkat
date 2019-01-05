4d1622a40c23eba090515f292e1f5cdc6fc910aa
ravi.yamula227 <ravi.yamula227@gmail.com>
2019-01-05 11:44:50 +0530

    commit

---
+++ src/full/Fulltogit.java
@@ -0,0 +1,15 @@
+package full;
+
+public class Fulltogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="venkat";
+String s2="ravi";
+String s3="raju";
+System.out.println(s);
+System.out.println(s2);
+System.out.println(s3);
+	}
+
+}
